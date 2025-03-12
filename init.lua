c51f43ea888de63eec0572f49acbb91279bed114
brocodzhang <brocodzhang@tencent.com>
2025-03-12 14:50:29 +0800

    add catppucin theme

--- init.lua
+++ init.lua
@@ -12,7 +12,7 @@ local options = {
   ignorecase = true,                       -- ignore case in search patterns
   mouse = "a",                             -- allow the mouse to be used in neovim
   pumheight = 10,                          -- pop up menu height
-  showmode = false,                        -- we don't need to see things like -- INSERT -- anymore
+  showmode = false,                        -- we don"t need to see things like -- INSERT -- anymore
   showtabline = 2,                         -- always show tabs
   smartcase = true,                        -- smart case
   smartindent = true,                      -- make indenting smarter again
@@ -34,7 +34,7 @@ local options = {
 
   signcolumn = "yes",                      -- always show the sign column, otherwise it would shift the text each time
   wrap = false,                            -- display lines as one long line
-  linebreak = true,                        -- companion to wrap, don't split words
+  linebreak = true,                        -- companion to wrap, don"t split words
   scrolloff = 4,                           -- minimal number of screen lines to keep above and below the cursor
   sidescrolloff = 4,                       -- minimal number of screen columns either side of cursor if wrap is `false`
   guifont = "monospace:h17",               -- the font used in graphical neovim applications
@@ -45,10 +45,10 @@ for k, v in pairs(options) do
   vim.opt[k] = v
 end
 
--- vim.opt.shortmess = "ilmnrx"                        -- flags to shorten vim messages, see :help 'shortmess'
-vim.opt.shortmess:append "c"                           -- don't give |ins-completion-menu| messages
+-- vim.opt.shortmess = "ilmnrx"                        -- flags to shorten vim messages, see :help "shortmess"
+vim.opt.shortmess:append "c"                           -- don"t give |ins-completion-menu| messages
 vim.opt.iskeyword:append "-"                           -- hyphenated words recognized by searches
-vim.opt.formatoptions:remove({ "c", "r", "o" })        -- don't insert the current comment leader automatically for auto-wrapping comments using 'textwidth', hitting <Enter> in insert mode, or hitting 'o' or 'O' in normal mode.
+vim.opt.formatoptions:remove({ "c", "r", "o" })        -- don"t insert the current comment leader automatically for auto-wrapping comments using "textwidth", hitting <Enter> in insert mode, or hitting "o" or "O" in normal mode.
 vim.opt.runtimepath:remove("/usr/share/vim/vimfiles")  -- separate vim plugins from neovim in case vim still in use
 
 -- --------------------------- keymaps ---------------------------------------
@@ -147,7 +147,7 @@ require("lazy").setup({
         "plenary.nvim",
       },
       config = function()
-        local config = require('session_manager.config')
+        local config = require("session_manager.config")
         require("session_manager").setup {
           autoload_mode = config.AutoloadMode.CurrentDir
         }
@@ -170,26 +170,34 @@ require("lazy").setup({
       end,
     },
     {
-      'nvim-telescope/telescope.nvim',
-      tag = '0.1.8',
-      dependencies = { 'nvim-lua/plenary.nvim' },
+      "nvim-telescope/telescope.nvim",
+      tag = "0.1.8",
+      dependencies = { "nvim-lua/plenary.nvim" },
       config = function()
-        local builtin = require('telescope.builtin')
-        vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
-        vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
-        vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
-        vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+        local builtin = require("telescope.builtin")
+        vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
+        vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" })
+        vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" })
+        vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope help tags" })
       end,
     },
     {
-      'akinsho/bufferline.nvim',
+      "akinsho/bufferline.nvim",
       version = "*",
-      dependencies = 'nvim-tree/nvim-web-devicons',
+      dependencies = "nvim-tree/nvim-web-devicons",
       config = function()
         require("bufferline").setup{}
         keymap("n", "<Tab>", ":BufferLineCycleNext<CR>", opts)
         keymap("n", "<S-Tab>", ":BufferLineCyclePrev<CR>", opts)
         keymap("n", "qq", ":bd<CR>", opts)
+      end,
+    },
+    {
+      "catppuccin/nvim",
+      name = "catppuccin",
+      priority = 1000,
+      config = function()
+        vim.cmd.colorscheme "catppuccin"
       end,
     },
   },
