bd01185f53a3de5dbe4f2dedc93b76bc7d3909f0
brocodzhang <brocodzhang@tencent.com>
2025-03-12 14:09:01 +0800

    add markdown treesitter

--- init.lua
+++ init.lua
@@ -161,7 +161,7 @@ require("lazy").setup({
         local configs = require("nvim-treesitter.configs")
 
         configs.setup({
-            ensure_installed = { "c", "cpp", "java", "lua", "vim", "vimdoc", "javascript", "html" },
+            ensure_installed = { "c", "cpp", "java", "lua", "vim", "vimdoc", "javascript", "html", "markdown" },
             sync_install = false,
             highlight = { enable = true },
             indent = { enable = true },  
